16f3de89264fbd7aed27b9ceb2309e871c193516
Chris Simpkins <git.simpkins@gmail.com>
2013-06-23 21:11:11 -0400

    added JSON pretty printing files

---
+++ tweetledee/homejson_pp.php
@@ -0,0 +1,107 @@
+<?php
+/***********************************************************************************************
+ * Tweetledee  - Incredibly easy access to Twitter data
+ *   homejson_pp.php -- Home timeline results formatted as pretty printed JSON
+ *   Version: 0.2.7
+ * Copyright 2013 Christopher Simpkins
+ * MIT License
+ ************************************************************************************************/
+/*-----------------------------------------------------------------------------------------------
+==> Instructions:
+    - place the tweetledee directory in the public facing directory on your web server (frequently public_html)
+    - Access the default home timeline JSON (count = 25 & includes replies) at the following URL:
+            e.g. http://<yourdomain>/tweetledee/homejson.php
+==> User Timeline JSON parameters:
+    - 'c' - specify a tweet count (range 1 - 200, default = 25)
+            e.g. http://<yourdomain>/tweetledee/homejson.php?c=100
+    - 'xrp' - exclude replies (1=true, default = false)
+            e.g. http://<yourdomain>/tweetledee/homejson.php?xrp=1
+    - Example of all of the available parameters:
+            e.g. http://<yourdomain>/tweetledee/homejson.php?c=100&xrp=1
+--------------------------------------------------------------------------------------------------*/
+/* Requirements:
+*       - requires PHP version 5.4 or greater (JSON_PRETTY_PRINT)
+*/
+
+// debugging
+$TLD_DEBUG = 0;
+if ($TLD_DEBUG == 1){
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL | E_STRICT);
+}
+
+// Matt Harris' Twitter OAuth library
+require 'tldlib/tmhOAuth.php';
+require 'tldlib/tmhUtilities.php';
+
+// include user keys
+require 'tldlib/keys/tweetledee_keys.php';
+
+// include Geoff Smith's utility functions
+require 'tldlib/tldUtilities.php';
+
+// create the OAuth object
+$tmhOAuth = new tmhOAuth(array(
+            'consumer_key'        => $my_consumer_key,
+            'consumer_secret'     => $my_consumer_secret,
+            'user_token'          => $my_access_token,
+            'user_secret'         => $my_access_token_secret,
+            'curl_ssl_verifypeer' => false
+        ));
+//*/
+
+// request the user information
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/account/verify_credentials')
+          )
+        );
+
+// Display error response if do not receive 200 response code
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("verify_credentials connection failure");
+}
+
+// Decode JSON
+$data = json_decode($tmhOAuth->response['response'], true);
+
+// Defaults
+$count = 25;  //default tweet number = 25
+$exclude_replies = false;  //default to include replies
+$screen_name = $data['screen_name'];
+
+// Parameters
+// c = tweet count ( possible range 1 - 200 tweets, else default = 25)
+if (isset($_GET["c"])){
+    $getcount = $_GET["c"];
+    if ($getcount > 0 && $getcount <= 200){
+        $count = $getcount;
+    }
+}
+
+// xrp = exclude replies from the timeline (possible values: 1=true, else false)
+if (isset($_GET["xrp"])){
+    if ($_GET["xrp"] == 1){
+        $exclude_replies = true;
+    }
+}
+
+// request the user timeline using the paramaters that were parsed from URL or that are defaults
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/statuses/home_timeline'),
+			'params' => array(
+          		'include_entities' => true,
+    			'count' => $count,
+    			'exclude_replies' => $exclude_replies,
+        	)
+        ));
+
+// Anything except code 200 is a failure to get the information
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("home_timeline connection failure");
+}
+
+$homeTimelineObj = json_decode($tmhOAuth->response['response'], true);
+header('Content-Type: application/json');
+echo json_encode($homeTimelineObj, JSON_PRETTY_PRINT);
---
+++ tweetledee/searchjson_pp.php
@@ -0,0 +1,110 @@
+<?php
+/***********************************************************************************************
+ * Tweetledee  - Incredibly easy access to Twitter data
+ *   searchjson_pp.php -- Search query results formatted as pretty printed JSON
+ *   Version: 0.2.7
+ * Copyright 2013 Christopher Simpkins
+ * MIT License
+ ************************************************************************************************/
+/*-----------------------------------------------------------------------------------------------
+==> Instructions:
+    - place the tweetledee directory in the public facing directory on your web server (frequently public_html)
+    - Access the default user timeline feed (count = 25, includes both RT's & replies) at the following URL:
+            e.g. http://<yourdomain>/twitter/userrss.php
+==> User Timeline RSS feed parameters:
+    - 'c' - specify a tweet count (range 1 - 200, default = 25)
+    - 'rt' - result type (possible values: mixed, recent, popular; default = mixed)
+    - 'q' - query term
+            e.g. http://<yourdomain>/twitter/searchrss.php?q=coolsearch&c=50
+--------------------------------------------------------------------------------------------------*/
+
+// debugging
+$TLD_DEBUG = 0;
+if ($TLD_DEBUG == 1){
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL | E_STRICT);
+}
+
+// Matt Harris' Twitter OAuth library
+require 'tldlib/tmhOAuth.php';
+require 'tldlib/tmhUtilities.php';
+
+// include user keys
+require 'tldlib/keys/tweetledee_keys.php';
+
+// include Geoff Smith's utility functions
+require 'tldlib/tldUtilities.php';
+
+// create the OAuth object
+///*
+$tmhOAuth = new tmhOAuth(array(
+            'consumer_key'        => $my_consumer_key,
+            'consumer_secret'     => $my_consumer_secret,
+            'user_token'          => $my_access_token,
+            'user_secret'         => $my_access_token_secret,
+            'curl_ssl_verifypeer' => false
+        ));
+//*/
+
+// request the user information
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/account/verify_credentials')
+          )
+        );
+
+// Display error response if do not receive 200 response code
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("verify_credentials connection failure");
+}
+
+// Decode JSON
+$data = json_decode($tmhOAuth->response['response'], true);
+
+// Defaults
+$count = 25;  //default tweet number = 25
+$result_type = 'mixed'; //default to mixed popular and realtime results
+
+// Parameters
+// c = tweet count ( possible range 1 - 200 tweets, else default = 25)
+if (isset($_GET["c"])){
+    if ($_GET["c"] > 0 && $_GET["c"] <= 200){
+        $count = $_GET["c"];
+    }
+}
+// rt = response type
+if (isset($_GET["rt"])){
+    if ($_GET["rt"] == 'popular' || $_GET["rt"] == 'recent'){
+        $result_type = $_GET["rt"];
+    }
+}
+// q = query
+if (isset($_GET["q"])){
+    $query = $_GET["q"];
+}
+else{
+    echo("The request was missing a search query");
+    die(" - search query missing failure");
+}
+$urlquery = urlencode($query);
+
+// request the user timeline using the paramaters that were parsed from URL or that are defaults
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/search/tweets'),
+			'params' => array(
+          		'include_entities' => true,
+    			'count' => $count,
+                'result_type' => $result_type,
+                'q' => $urlquery,
+        	)
+        ));
+
+// Anything except code 200 is a failure to get the information
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("tweet_search connection failure");
+}
+
+$searchResultsObj = json_decode($tmhOAuth->response['response'], true);
+header('Content-Type: application/json');
+echo json_encode($searchResultsObj, JSON_PRETTY_PRINT);
