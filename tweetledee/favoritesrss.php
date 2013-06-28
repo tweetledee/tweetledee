93a03cc5419948679a5289ee781a0a5b992f55c0
Chris Simpkins <git.simpkins@gmail.com>
2013-06-27 22:07:09 -0400

    updated version

--- tweetledee/favoritesrss.php
+++ tweetledee/favoritesrss.php
@@ -2,7 +2,8 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   favoritesrss.php -- User favorites formatted as a RSS feed
- *   Version: 0.2.7
+ *   Version: 0.2.8
+ * Copyright 2013 George Dorn & Christopher Simpkins
  * MIT License
  ************************************************************************************************/
 /*-----------------------------------------------------------------------------------------------
