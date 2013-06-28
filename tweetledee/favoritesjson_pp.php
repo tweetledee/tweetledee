ae3b85ab5e31ba0b95c0d920c0b87b8301452862
Chris Simpkins <git.simpkins@gmail.com>
2013-06-27 22:08:41 -0400

    added new user favorites JSON and user favorites pretty printed JSON files

---
+++ tweetledee/favoritesjson_pp.php
@@ -0,0 +1,102 @@
+<?php
+/***********************************************************************************************
+ * Tweetledee  - Incredibly easy access to Twitter data
+ *   favoritesjson_pp.php -- User favorites formatted as pretty printed JSON
+ *   Version: 0.2.8
+ * Copyright 2013 Christopher Simpkins
+ * MIT License
+ ************************************************************************************************/
+/*-----------------------------------------------------------------------------------------------
+==> Instructions:
+    - place the tweetledee directory in the public facing directory on your web server (frequently public_html)
+    - Access the default user favorites feed (count = 25, includes both RT's & replies) at the following URL:
+            e.g. http://<yourdomain>/tweetledee/favoritesjson_pp.php
+==> User Favorites RSS feed parameters:
+    - 'c' - specify a tweet count (range 1 - 200, default = 25)
+            e.g. http://<yourdomain>/tweetledee/favoritesjson_pp.php?c=100
+    - 'user' - specify the Twitter user whose favorites you would like to retrieve (default = account associated with access token)
+            e.g. http://<yourdomain>/tweetledee/favoritesjson_pp.php?user=cooluser
+    - Example of all of the available parameters:
+            e.g. http://<yourdomain>/tweetledee/favoritesjson_pp.php?c=100&user=cooluser
+--------------------------------------------------------------------------------------------------*/
+// debugging
+$TLD_DEBUG = 0;
+if ($TLD_DEBUG == 1){
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL | E_STRICT);
+}
+
+// Matt Harris' Twitter OAuth library
+require 'tldlib/tmhOAuth.php';
+require 'tldlib/tmhUtilities.php';
+
+// include user keys
+require 'tldlib/keys/tweetledee_keys.php';
+
+// include Geoff Smith's utility functions
+require 'tldlib/tldUtilities.php';
+
+// create the OAuth object
+///*
+$tmhOAuth = new tmhOAuth(array(
+            'consumer_key'        => $my_consumer_key,
+            'consumer_secret'     => $my_consumer_secret,
+            'user_token'          => $my_access_token,
+            'user_secret'         => $my_access_token_secret,
+            'curl_ssl_verifypeer' => false
+        ));
+//*/
+
+// request the user information
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/account/verify_credentials')
+          )
+        );
+
+// Display error response if do not receive 200 response code
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("verify_credentials connection failure");
+}
+
+// Decode JSON
+$data = json_decode($tmhOAuth->response['response'], true);
+
+// Defaults
+$count = 25;  //default tweet number = 25
+$include_retweets = true;  //default to include retweets
+$exclude_replies = false;  //default to include replies
+$screen_name = $data['screen_name'];
+
+// Parameters
+// c = tweet count ( possible range 1 - 200 tweets, else default = 25)
+if (isset($_GET["c"])){
+    if ($_GET["c"] > 0 && $_GET["c"] <= 200){
+        $count = $_GET["c"];
+    }
+}
+
+// user = Twitter screen name for the user favorites that the user is requesting (default = their own, possible values = any other Twitter user name)
+if (isset($_GET["user"])){
+    $screen_name = $_GET["user"];
+}
+
+// request the user favorites using the paramaters that were parsed from URL or that are defaults
+$code = $tmhOAuth->user_request(array(
+			'url' => $tmhOAuth->url('1.1/favorites/list'),
+			'params' => array(
+          		'include_entities' => true,
+    			'count' => $count,
+    			'screen_name' => $screen_name,
+        	)
+        ));
+
+// Anything except code 200 is a failure to get the information
+if ($code <> 200) {
+    echo $tmhOAuth->response['error'];
+    die("user_favorites connection failure");
+}
+
+$userFavoritesObj = json_decode($tmhOAuth->response['response'], true);
+header('Content-Type: application/json');
+echo json_encode($userFavoritesObj, JSON_PRETTY_PRINT);
