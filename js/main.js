d1542bf0bf73ebbf8e0d47b564b04ba2f277f7d3
Chris Simpkins <git.simpkins@gmail.com>
2013-07-04 22:41:30 -0400

    added new js directory

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// GA download links
+$('#dl-btn-zip').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'download-link-zip');
+});
+
+$('#dl-btn-gz').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'download-link-gz');
+});
+
+
