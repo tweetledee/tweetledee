12d78712850df8e3712cffb564837dbf9f76c2a7
Chris Simpkins <git.simpkins@gmail.com>
2013-07-10 18:20:42 -0400

    search JSON, pp JSON, RSS files updated with CLI/SSH access, data through standard out stream

--- tweetledee/searchjson.php
+++ tweetledee/searchjson.php
@@ -1,29 +1,38 @@
 <?php
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
- *   searchjson.php -- Search query results formatted as JSON
- *   Version: 0.2.9
+ *   searchjson.php -- Tweet search query results formatted as JSON
+ *   Version: 0.3.0
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
 /*-----------------------------------------------------------------------------------------------
 ==> Instructions:
     - place the tweetledee directory in the public facing directory on your web server (frequently public_html)
-    - Access the default user timeline feed (count = 25, includes both RT's & replies) at the following URL:
-            e.g. http://<yourdomain>/tweetledee/userrss.php
-==> User Timeline RSS feed parameters:
+    - Generic tweet search JSON URL (count = 25):
+            e.g. http://<yourdomain>/tweetledee/searchjson.php?q=<search-term>
+==> Twitter Tweet Search JSON parameters:
     - 'c' - specify a tweet count (range 1 - 200, default = 25)
+            e.g. http://<yourdomain>/tweetledee/searchjson.php?q=<search-term>&c=100
     - 'rt' - result type (possible values: mixed, recent, popular; default = mixed)
+            e.g. http://<yourdomain>/tweetledee/searchjson.php?q=<search-term>&rt=recent
     - 'q' - query term
-            e.g. http://<yourdomain>/tweetledee/searchrss.php?q=coolsearch&c=50
+            e.g. http://<yourdomain>/tweetledee/searchjson.php?q=coolsearch
+    -  Example of all parameters
+            http://<yourdomain>/tweetledee/searchjson.php?q=coolsearch&c=50&rt=recent
 --------------------------------------------------------------------------------------------------*/
-// debugging
+/*******************************************************************
+*  Debugging Flag
+********************************************************************/
 $TLD_DEBUG = 0;
 if ($TLD_DEBUG == 1){
     ini_set('display_errors', 'On');
     error_reporting(E_ALL | E_STRICT);
 }
 
+/*******************************************************************
+*  Includes
+********************************************************************/
 // Matt Harris' Twitter OAuth library
 require 'tldlib/tmhOAuth.php';
 require 'tldlib/tmhUtilities.php';
@@ -34,8 +43,36 @@ require 'tldlib/keys/tweetledee_keys.php';
 // include Geoff Smith's utility functions
 require 'tldlib/tldUtilities.php';
 
-// create the OAuth object
-///*
+/***************************************************************************************
+*  Mandatory parameter (q)
+*   - do not execute the OAuth authentication request if missing (keep before OAuth code)
+****************************************************************************************/
+// q = search query term
+if (isset($_GET["q"])){
+    $query = $_GET["q"];
+}
+else if (defined('STDIN')) {
+    if (isset($argv)){
+        $shortopts = "q:";
+    }
+    else {
+        die("Error: missing the search query term in your request.  Please use the 'q' parameter in your request.");
+    }
+    $params = getopt($shortopts);
+    if (isset($params['q'])){
+        $query = $params['q'];
+    }
+    else{
+        die("Error: unable to parse the search query term in your request.  Please use the 'q' parameter in your request.");
+    }
+}
+else{
+    die("Error: missing search query term in your request.  Please use the 'q' parameter in your request.");
+}
+
+/*******************************************************************
+*  OAuth
+********************************************************************/
 $tmhOAuth = new tmhOAuth(array(
             'consumer_key'        => $my_consumer_key,
             'consumer_secret'     => $my_consumer_secret,
@@ -43,7 +80,6 @@ $tmhOAuth = new tmhOAuth(array(
             'user_secret'         => $my_access_token_secret,
             'curl_ssl_verifypeer' => false
         ));
-//*/
 
 // request the user information
 $code = $tmhOAuth->user_request(array(
@@ -63,35 +99,58 @@ if ($code <> 200) {
 // Decode JSON
 $data = json_decode($tmhOAuth->response['response'], true);
 
-// Defaults
+/*******************************************************************
+*  Defaults
+********************************************************************/
 $count = 25;  //default tweet number = 25
 $result_type = 'mixed'; //default to mixed popular and realtime results
 
-// Parameters
-// c = tweet count ( possible range 1 - 200 tweets, else default = 25)
-if (isset($_GET["c"])){
-    if ($_GET["c"] > 0 && $_GET["c"] <= 200){
-        $count = $_GET["c"];
+/*******************************************************************
+*   Optional Parameters
+*    - can pass via URL to web server
+*    - or as a short or long switch at the command line
+********************************************************************/
+
+// Command line parameter definitions //
+if (defined('STDIN')) {
+    // check whether arguments were passed, if not there is no need to attempt to check the array
+    if (isset($argv)){
+        $shortopts = "c:";
+        $longopts = array(
+            "rt",
+        );
+        $params = getopt($shortopts, $longopts);
+        if (isset($params['c'])){
+            if ($params['c'] > 0 && $params['c'] < 200)
+                $count = $params['c'];  //assign to the count variable
+        }
+        if (isset($params['rt'])){
+            $result_type = $params['rt'];
+        }
     }
 }
-// rt = response type
-if (isset($_GET["rt"])){
-    if ($_GET["rt"] == 'popular' || $_GET["rt"] == 'recent'){
-        $result_type = $_GET["rt"];
-    }
-}
-// q = query
-if (isset($_GET["q"])){
-    $query = $_GET["q"];
-}
+// Web server URL parameter definitions //
 else{
-    echo("The request was missing a search query");
-    die(" - search query missing failure");
+    // c = tweet count ( possible range 1 - 200 tweets, else default = 25)
+    if (isset($_GET["c"])){
+        if ($_GET["c"] > 0 && $_GET["c"] <= 200){
+            $count = $_GET["c"];
+        }
+    }
+    // rt = response type
+    if (isset($_GET["rt"])){
+        if ($_GET["rt"] == 'popular' || $_GET["rt"] == 'recent'){
+            $result_type = $_GET["rt"];
+        }
+    }
 }
 
+//url encode the search query
 $urlquery = urlencode($query);
 
-// request the user timeline using the paramaters that were parsed from URL or that are defaults
+/*******************************************************************
+*  Request
+********************************************************************/
 $code = $tmhOAuth->user_request(array(
 			'url' => $tmhOAuth->url('1.1/search/tweets'),
 			'params' => array(
--- tweetledee/searchjson_pp.php
+++ tweetledee/searchjson_pp.php
@@ -1,30 +1,39 @@
 <?php
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
- *   searchjson_pp.php -- Search query results formatted as pretty printed JSON
- *   Version: 0.2.9
+ *   searchjson_pp.php -- Tweet search query results formatted as pretty printed JSON
+ *   Version: 0.3.0
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
 /*-----------------------------------------------------------------------------------------------
 ==> Instructions:
     - place the tweetledee directory in the public facing directory on your web server (frequently public_html)
-    - Access the default user timeline feed (count = 25, includes both RT's & replies) at the following URL:
-            e.g. http://<yourdomain>/tweetledee/userrss.php
-==> User Timeline RSS feed parameters:
+    - Generic tweet search pretty printed JSON URL (count = 25):
+            e.g. http://<yourdomain>/tweetledee/searchjson_pp.php?q=<search-term>
+==> Twitter Tweet Search Pretty Printed JSON parameters:
     - 'c' - specify a tweet count (range 1 - 200, default = 25)
+            e.g. http://<yourdomain>/tweetledee/searchjson_pp.php?q=<search-term>&c=100
     - 'rt' - result type (possible values: mixed, recent, popular; default = mixed)
+            e.g. http://<yourdomain>/tweetledee/searchjson_pp.php?q=<search-term>&rt=recent
     - 'q' - query term
-            e.g. http://<yourdomain>/tweetledee/searchrss.php?q=coolsearch&c=50
+            e.g. http://<yourdomain>/tweetledee/searchjson_pp.php?q=coolsearch
+    - Example of all parameters:
+            http://<yourdomain>/tweetledee/searchjson_pp.php?q=coolsearch&c=50&rt=recent
 --------------------------------------------------------------------------------------------------*/
 
-// debugging
+/*******************************************************************
+*  Debugging Flag
+********************************************************************/
 $TLD_DEBUG = 0;
 if ($TLD_DEBUG == 1){
     ini_set('display_errors', 'On');
     error_reporting(E_ALL | E_STRICT);
 }
 
+/*******************************************************************
+*  Includes
+********************************************************************/
 // Matt Harris' Twitter OAuth library
 require 'tldlib/tmhOAuth.php';
 require 'tldlib/tmhUtilities.php';
@@ -35,8 +44,36 @@ require 'tldlib/keys/tweetledee_keys.php';
 // include Geoff Smith's utility functions
 require 'tldlib/tldUtilities.php';
 
-// create the OAuth object
-///*
+/***************************************************************************************
+*  Mandatory parameter (q)
+*   - do not execute the OAuth authentication request if missing (keep before OAuth code)
+****************************************************************************************/
+// q = search query term
+if (isset($_GET["q"])){
+    $query = $_GET["q"];
+}
+else if (defined('STDIN')) {
+    if (isset($argv)){
+        $shortopts = "q:";
+    }
+    else {
+        die("Error: missing the search query term in your request.  Please use the 'q' parameter in your request.");
+    }
+    $params = getopt($shortopts);
+    if (isset($params['q'])){
+        $query = $params['q'];
+    }
+    else{
+        die("Error: unable to parse the search query term in your request.  Please use the 'q' parameter in your request.");
+    }
+}
+else{
+    die("Error: missing search query term in your request.  Please use the 'q' parameter in your request.");
+}
+
+/*******************************************************************
+*  OAuth
+********************************************************************/
 $tmhOAuth = new tmhOAuth(array(
             'consumer_key'        => $my_consumer_key,
             'consumer_secret'     => $my_consumer_secret,
@@ -44,11 +81,10 @@ $tmhOAuth = new tmhOAuth(array(
             'user_secret'         => $my_access_token_secret,
             'curl_ssl_verifypeer' => false
         ));
-//*/
 
 // request the user information
 $code = $tmhOAuth->user_request(array(
-			'url' => $tmhOAuth->url('1.1/account/verify_credentials')
+            'url' => $tmhOAuth->url('1.1/account/verify_credentials')
           )
         );
 
@@ -64,42 +100,66 @@ if ($code <> 200) {
 // Decode JSON
 $data = json_decode($tmhOAuth->response['response'], true);
 
-// Defaults
+/*******************************************************************
+*  Defaults
+********************************************************************/
 $count = 25;  //default tweet number = 25
 $result_type = 'mixed'; //default to mixed popular and realtime results
 
-// Parameters
-// c = tweet count ( possible range 1 - 200 tweets, else default = 25)
-if (isset($_GET["c"])){
-    if ($_GET["c"] > 0 && $_GET["c"] <= 200){
-        $count = $_GET["c"];
+/*******************************************************************
+*   Optional Parameters
+*    - can pass via URL to web server
+*    - or as a short or long switch at the command line
+********************************************************************/
+
+// Command line parameter definitions //
+if (defined('STDIN')) {
+    // check whether arguments were passed, if not there is no need to attempt to check the array
+    if (isset($argv)){
+        $shortopts = "c:";
+        $longopts = array(
+            "rt",
+        );
+        $params = getopt($shortopts, $longopts);
+        if (isset($params['c'])){
+            if ($params['c'] > 0 && $params['c'] < 200)
+                $count = $params['c'];  //assign to the count variable
+        }
+        if (isset($params['rt'])){
+            $result_type = $params['rt'];
+        }
     }
 }
-// rt = response type
-if (isset($_GET["rt"])){
-    if ($_GET["rt"] == 'popular' || $_GET["rt"] == 'recent'){
-        $result_type = $_GET["rt"];
-    }
-}
-// q = query
-if (isset($_GET["q"])){
-    $query = $_GET["q"];
-}
+// Web server URL parameter definitions //
 else{
-    echo("The request was missing a search query");
-    die(" - search query missing failure");
+    // c = tweet count ( possible range 1 - 200 tweets, else default = 25)
+    if (isset($_GET["c"])){
+        if ($_GET["c"] > 0 && $_GET["c"] <= 200){
+            $count = $_GET["c"];
+        }
+    }
+    // rt = response type
+    if (isset($_GET["rt"])){
+        if ($_GET["rt"] == 'popular' || $_GET["rt"] == 'recent'){
+            $result_type = $_GET["rt"];
+        }
+    }
 }
+
+//url encode the search query
 $urlquery = urlencode($query);
 
-// request the user timeline using the paramaters that were parsed from URL or that are defaults
+/*******************************************************************
+*  Request
+********************************************************************/
 $code = $tmhOAuth->user_request(array(
-			'url' => $tmhOAuth->url('1.1/search/tweets'),
-			'params' => array(
-          		'include_entities' => true,
-    			'count' => $count,
+            'url' => $tmhOAuth->url('1.1/search/tweets'),
+            'params' => array(
+                'include_entities' => true,
+                'count' => $count,
                 'result_type' => $result_type,
                 'q' => $urlquery,
-        	)
+            )
         ));
 
 // Anything except code 200 is a failure to get the information
