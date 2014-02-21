a8e64afe458bbe422ceee7d9b711525044e1ba68
levymetal <christian@varga.co>
2014-02-21 15:32:43 +1100

    Update userrss.php

--- tweetledee/userrss.php
+++ tweetledee/userrss.php
@@ -2,7 +2,7 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   userrss.php -- User timeline results formatted as a RSS feed
- *   Version: 0.4.6
+ *   Version: 0.3.7
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
