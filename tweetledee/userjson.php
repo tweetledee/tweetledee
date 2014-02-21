2c9bf885f4f5902a242673d935c85374df8990b1
levymetal <christian@varga.co>
2014-02-21 15:32:35 +1100

    Update userjson.php

--- tweetledee/userjson.php
+++ tweetledee/userjson.php
@@ -2,7 +2,7 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   userjson.php -- User timeline results formatted as JSON
- *   Version: 0.4.6
+ *   Version: 0.3.7
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
