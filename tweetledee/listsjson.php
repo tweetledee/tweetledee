62bd9145c2bafdd8b91233b8b54a0ac2bebfcca9
levymetal <christian@varga.co>
2014-02-21 15:33:02 +1100

    Update listsjson.php

--- tweetledee/listsjson.php
+++ tweetledee/listsjson.php
@@ -2,7 +2,7 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   listsjson.php -- User list tweets formatted as JSON
- *   Version: 0.4.6
+ *   Version: 0.3.7
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
