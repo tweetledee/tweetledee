2643f992f520134e379af47677a92363bb7a47f5
levymetal <christian@varga.co>
2014-02-21 15:32:21 +1100

    Update homerss.php

--- tweetledee/homerss.php
+++ tweetledee/homerss.php
@@ -2,7 +2,7 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   homerss.php -- Home timeline results formatted as RSS feed
- *   Version: 0.4.6
+ *   Version: 0.3.7
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
