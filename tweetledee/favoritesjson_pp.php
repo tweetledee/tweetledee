1d71c6af797847cd3ff5b5252e56300910fe5a59
levymetal <christian@varga.co>
2014-02-21 15:32:00 +1100

    Update favoritesjson_pp.php

--- tweetledee/favoritesjson_pp.php
+++ tweetledee/favoritesjson_pp.php
@@ -2,7 +2,7 @@
 /***********************************************************************************************
  * Tweetledee  - Incredibly easy access to Twitter data
  *   favoritesjson_pp.php -- User favorites formatted as pretty printed JSON
- *   Version: 0.4.6
+ *   Version: 0.3.7
  * Copyright 2013 Christopher Simpkins
  * MIT License
  ************************************************************************************************/
