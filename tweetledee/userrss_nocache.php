fd07aabf9be77c6befa10a8a233f28ceba72888e
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2017-06-01 10:43:30 +0200

    Fix: images from other tweets are shown.

--- tweetledee/userrss_nocache.php
+++ tweetledee/userrss_nocache.php
@@ -215,9 +215,6 @@ header("Content-type: text/xml; charset=utf-8");
                     $tweeter = $currentitem['user']['screen_name'];
                     $fullname = $currentitem['user']['name'];
                     $tweetTitle = $currentitem['text'];
-                    if(isset($currentitem['entities']['media'][0]['media_url'])):
-                    $picurl = $currentitem['entities']['media'][0]['media_url'];
-                    endif;
                endif;
                 ?>
 				<title>[<?php echo $tweeter; ?>] <?php echo $tweetTitle; ?> </title>
@@ -235,7 +232,7 @@ header("Content-type: text/xml; charset=utf-8");
                         <strong><?php echo $fullname; ?></strong> <a href='https://twitter.com/<?php echo $tweeter; ?>' target='blank'>@<?php echo $tweeter;?></a><?php echo $rt ?><br />
                         <?php echo $parsedTweet; ?>
                         <?php if(isset($currentitem['entities']['media'][0]['media_url'])): ?>
-                        <img src='<?php echo $picurl; ?>' border=0 />
+                        <img src='<?php echo $currentitem['entities']['media'][0]['media_url']; ?>' border=0 />
                         <?php endif; ?>
                     ]]>
                </description>
