9bdf9968976d60b8ab249c2b44cad493a05d6e32
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2018-04-17 00:08:50 +0200

    Configure the app with environment variables.

--- tweetledee/tldlib/keys/tweetledee_keys.php
+++ tweetledee/tldlib/keys/tweetledee_keys.php
@@ -1,5 +1,13 @@
 <?php
 
+// Configure tweetledee with environment variables.
+$my_consumer_key        = getenv('TWEETLEDEE_CONSUMER_KEY');
+$my_consumer_secret     = getenv('TWEETLEDEE_CONSUMER_SECRET');
+$my_access_token        = getenv('TWEETLEDEE_ACCESS_TOKEN');
+$my_access_token_secret = getenv('TWEETLEDEE_ACCESS_TOKEN_SECRET');
+$my_domain              = getenv('TWEETLEDEE_DOMAIN');
+
+
 /*
 * PLEASE READ THE FOLLOWING
 * Enter your Twitter application information in the appropriate lines below this comment section.
@@ -18,14 +26,12 @@
 *  OK, now it's your turn.  I used ALL CAPS where you should insert your information.  If it's not in ALL CAPS, it's a good bet
 *  that you should not modify it.
 *
-*  Enter your information below this line:
+*  Uncomment and enter your information below this line:
 */
 
-$my_consumer_key        = 'ENTER YOUR CONSUMER KEY';
-$my_consumer_secret     = 'ENTER YOUR CONSUMER SECRET';
-$my_access_token        = 'ENTER YOUR ACCESS TOKEN';
-$my_access_token_secret = 'ENTER YOUR ACCESS TOKEN SECRET';
-$my_domain              = 'ENTER YOUR DOMAIN NAME';
-
-
+// $my_consumer_key        = 'ENTER YOUR CONSUMER KEY';
+// $my_consumer_secret     = 'ENTER YOUR CONSUMER SECRET';
+// $my_access_token        = 'ENTER YOUR ACCESS TOKEN';
+// $my_access_token_secret = 'ENTER YOUR ACCESS TOKEN SECRET';
+// $my_domain              = 'ENTER YOUR DOMAIN NAME';
 ?>
