38a6d31e68981ce090dc68c55638f771b096ec13
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-10-09 11:42:01 +0200

    Used renderer to simplify all rss outputting pages for #68

--- tweetledee/searchrss.php
+++ tweetledee/searchrss.php
@@ -46,6 +46,9 @@ require 'tldlib/tldUtilities.php';
 // include Christian Varga's twitter cache
 require 'tldlib/tldCache.php';
 
+require 'tldlib/renderers/rss.php';
+
+
 /***************************************************************************************
 *  Mandatory parameter (q)
 *   - do not execute the OAuth authentication request if missing (keep before OAuth code)
@@ -175,70 +178,15 @@ if (defined('STDIN')) {
 // Start the output
 header("Content-Type: application/rss+xml");
 header("Content-type: text/xml; charset=utf-8");
+
+$renderer = new RssRenderer();
+$config = array(
+    'atom'              =>  $my_domain . urlencode($thequery),
+    'link'               =>  sprintf('http://www.twitter.com/search/?q=%s', $query),
+    'lastBuildDate'     =>  date(DATE_RSS),
+    'title'             =>  $feedTitle,
+    'description'       =>  sprintf('A Twitter search for the query "%s" with the %s search result type', $query, $result_type),
+    'twitterAvatarUrl'  =>  $twitterAvatarUrl,
+);
 ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-        <atom:link href="<?php echo $my_domain; ?><?php echo urlencode($thequery); ?>" rel="self" type="application/rss+xml" />
-        <lastBuildDate><?php echo date(DATE_RSS); ?></lastBuildDate>
-        <language>en</language>
-        <title><?php echo $feedTitle; ?></title>
-        <description>A Twitter search for the query "<?php echo $query; ?>" with the <?php echo $result_type; ?> search result type</description>
-        <link>http://www.twitter.com/search/?q=<?php echo $query; ?></link>
-        <ttl>960</ttl>
-        <generator>Tweetledee</generator>
-        <category>Personal</category>
-        <image>
-        <title><?php echo $feedTitle; ?></title>
-        <link>http://www.twitter.com/<?php echo $twitterName; ?></link>
-        <url>http://www.twitter.com/search/?q=<?php echo $query; ?></url>
-        </image>
-        <?php foreach ($searchResultsObj['statuses'] as $currentitem) : ?>
-            <item>
-                 <?php
-                 $parsedTweet = tmhUtilities::entify_with_options(
-                        objectToArray($currentitem),
-                        array(
-                            'target' => 'blank',
-                        )
-                 );
-
-                if (isset($currentitem['retweeted_status'])) :
-                    $avatar = $currentitem['retweeted_status']['user']['profile_image_url'];
-                    $rt = '&nbsp;&nbsp;&nbsp;&nbsp;[<em style="font-size:smaller;">Retweeted by ' . $currentitem['user']['screen_name'] . ' <a href=\'http://twitter.com/' . $currentitem['user']['screen_name'] . '\'>@' . $currentitem['user']['screen_name'] . '</a></em>]';
-                    $tweeter =  $currentitem['retweeted_status']['user']['screen_name'];
-                    $fullname = $currentitem['retweeted_status']['user']['name'];
-                    $tweetTitle = $currentitem['retweeted_status']['full_text'];
-                else :
-                    $avatar = $currentitem['user']['profile_image_url'];
-                    $rt = '';
-                    $tweeter = $currentitem['user']['screen_name'];
-                    $fullname = $currentitem['user']['name'];
-                    $tweetTitle = $currentitem['full_text'];
-                    if(isset($currentitem['entities']['media'][0]['media_url_https'])):
-                    $picurl = $currentitem['entities']['media'][0]['media_url_https'];
-                    endif;
-               endif;
-                ?>
-                <title>[<?php echo $tweeter; ?>] <?php echo $tweetTitle; ?> </title>
-                <pubDate><?php echo reformatDate($currentitem['created_at']); ?></pubDate>
-                <link>https://twitter.com/<?php echo $currentitem['user']['screen_name'] ?>/statuses/<?php echo $currentitem['id_str']; ?></link>
-                <guid isPermaLink='false'><?php echo $currentitem['id_str']; ?></guid>
-
-                <description>
-                    <![CDATA[
-                        <div style='float:left;margin: 0 6px 6px 0;'>
-                            <a href='https://twitter.com/<?php echo $tweeter ?>/statuses/<?php echo $currentitem['id_str']; ?>' border=0 target='blank'>
-                                <img src='<?php echo $avatar; ?>' border=0 />
-                            </a>
-                        </div>
-                        <strong><?php echo $fullname; ?></strong> <a href='https://twitter.com/<?php echo $tweeter; ?>' target='blank'>@<?php echo $tweeter;?></a><?php echo $rt ?><br />
-                        <?php echo $parsedTweet; ?>
-                        <?php if(isset($currentitem['entities']['media'][0]['media_url_https'])): ?>
-                        <img src='<?php echo $currentitem['entities']['media'][0]['media_url_https']; ?>' border=0 />
-                        <?php endif; ?>
-                    ]]>
-               </description>
-            </item>
-        <?php endforeach; ?>
-    </channel>
-</rss>
+<?php echo $renderer->render_feed($config, $searchResultsObj['statuses'])?>
