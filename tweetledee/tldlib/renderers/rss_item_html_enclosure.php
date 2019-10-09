2f6e5071ba7532e5a91cc665f0d7b37ba8fdc933
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-10-09 16:38:33 +0200

    For #67, separated item RSS datra and HTML enclosure

---
+++ tweetledee/tldlib/renderers/rss_item_html_enclosure.php
@@ -0,0 +1,17 @@
+<div style='float:left;margin: 0 6px 6px 0;'>
+	<a href='https://twitter.com/<?php echo $tweeter ?>/statuses/<?php echo $currentitem['id_str']; ?>' border=0 target='blank'>
+		<img src='<?php echo $avatar; ?>' border=0 />
+	</a>
+</div>
+<strong><?php echo $fullname; ?></strong> <a href='https://twitter.com/<?php echo $tweeter; ?>' target='blank'>@<?php echo $tweeter;?></a><?php echo $rt ?><br />
+<?php echo $parsedTweet; ?>
+<?php if(isset($currentitem['extended_entities']['media'])): ?>
+<?php foreach ($currentitem['extended_entities']['media'] as $entity):?>
+<img src='<?php echo $entity['media_url_https'] ?>' border=0 />
+<?php endforeach;?>
+<?php endif; ?>
+<?php if(count($currentitem['entities']['urls'])>0): ?>
+<?php foreach ($currentitem['entities']['urls'] as $included_tweet_url):?>
+
+<?php endforeach;?>
+<?php endif; ?>
