24920191ff2de15763d8bb06b183edbfdad80558
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2019-11-06 19:49:26 +0100

    Fix keys file.

--- tweetledee/tldlib/keys/tweetledee_keys.php
+++ tweetledee/tldlib/keys/tweetledee_keys.php
@@ -1,30 +1,37 @@
 <?php
 
+// Configure tweetledee with environment variables.
+$my_consumer_key        = getenv('TWEETLEDEE_CONSUMER_KEY');
+$my_consumer_secret     = getenv('TWEETLEDEE_CONSUMER_SECRET');
+$my_access_token        = getenv('TWEETLEDEE_ACCESS_TOKEN');
+$my_access_token_secret = getenv('TWEETLEDEE_ACCESS_TOKEN_SECRET');
+$my_domain              = getenv('TWEETLEDEE_DOMAIN');
+
+
 /*
- * PLEASE READ THE FOLLOWING
- * Enter your Twitter application information in the appropriate lines below this comment section.
- * Please leave the single quotes around the alphanumeric strings that you enter.
- *
- *  An appropriately formatted keys file should look like this:
- *       $my_consumer_key        = 'CoMpLetEly8oGusK3y';
- *       $my_consumer_secret     = 'SuPercaliFragilist1cEXPiaLidOCIous';
- *       $my_access_token        = 'AcC355MePLz';
- *       $my_access_token_secret = '5555shTh1s1iS53cr3t';
- *       $my_domain              = 'http://www.toolegittoquit.com';
- *
- *   Please note that the number of characters in the above examples are not necessarily correct and that these are bogus tokens.
- *   Users who have attempted a 'workaround' for their API limits received tweets from Mary Poppins and MC Hammer...
- *
- *  OK, now it's your turn.  I used ALL CAPS where you should insert your information.  If it's not in ALL CAPS, it's a good bet
- *  that you should not modify it.
- *
- *  Enter your information below this line:
- */
-/*
-$my_consumer_key        = '';
-$my_consumer_secret     = '';
-$my_access_token        = '';
-$my_access_token_secret = '';
-$my_domain              = 'http://nicolas-delsaux.hd.free.fr';
+* PLEASE READ THE FOLLOWING
+* Enter your Twitter application information in the appropriate lines below this comment section.
+* Please leave the single quotes around the alphanumeric strings that you enter.
+*
+*  An appropriately formatted keys file should look like this:
+*  	$my_consumer_key        = 'CoMpLetEly8oGusK3y';
+*	$my_consumer_secret     = 'SuPercaliFragilist1cEXPiaLidOCIous';
+*	$my_access_token        = 'AcC355MePLz';
+*	$my_access_token_secret = '5555shTh1s1iS53cr3t';
+*	$my_domain              = 'https://www.rodrigoaguilera.net';
+*
+*   Please note that the number of characters in the above examples are not necessarily correct and that these are bogus tokens.
+*   Users who have attempted a 'workaround' for their API limits received tweets from Mary Poppins and MC Hammer...
+*
+*  OK, now it's your turn.  I used ALL CAPS where you should insert your information.  If it's not in ALL CAPS, it's a good bet
+*  that you should not modify it.
+*
+*  Uncomment and enter your information below this line:
 */
+
+// $my_consumer_key        = 'ENTER YOUR CONSUMER KEY';
+// $my_consumer_secret     = 'ENTER YOUR CONSUMER SECRET';
+// $my_access_token        = 'ENTER YOUR ACCESS TOKEN';
+// $my_access_token_secret = 'ENTER YOUR ACCESS TOKEN SECRET';
+// $my_domain              = 'ENTER YOUR DOMAIN NAME';
 ?>
